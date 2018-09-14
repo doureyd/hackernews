7c51840c748220e01d316c9430a4b4b6699b01c8
daynox <doureyd@gmail.com>
2018-09-14 19:02:08 +0200

    Move archive action to Redux

--- src/index.js
+++ src/index.js
@@ -3,14 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import store from './store';
+import { STORY_ARCHIVE } from './constants/actionTypes';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
   <App
     stories={store.getState().storyState}
-    onArchive={objectID => {
-      console.log(objectID);
-    }}
+    onArchive={id => store.dispatch({ type: STORY_ARCHIVE, id })}
   />,
   document.getElementById('root')
 );
