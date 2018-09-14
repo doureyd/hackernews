cc6ed3d02c27c487e97441484f1be93f77bdfb17
daynox <doureyd@gmail.com>
2018-09-14 16:49:44 +0200

    Clean structure & Add basic components

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './App.css';
+
+import Stories from './Stories';
+
+const App = ({ stories }) => (
+  <div className="app">
+    <Stories stories={stories} />
+  </div>
+);
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const stories = [
+  {
+    title: 'Framer X - Interactive Design for Everyone',
+    url: 'https://framer.com/x/',
+    author: 'Framer',
+    num_comments: 13,
+    points: 7,
+    objectID: 0,
+  },
+  {
+    title: 'Sketch - The digital design toolkit',
+    url: 'https://www.sketchapp.com/',
+    author: 'Bohemian Coding',
+    num_comments: 19,
+    points: 6,
+    objectID: 1,
+  },
+];
+
+ReactDOM.render(<App stories={stories} />, document.getElementById('root'));
 registerServiceWorker();
